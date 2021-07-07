4ce90cbb96f53697ba28957e01c07a4c4d617cb8
KMS\user <epicblue@hanmail.net>
2021-07-07 19:59:05 +0900

    learned filesystem can use raw html file as an template. it sends unmodified buffers to client

---
+++ practice.js
@@ -0,0 +1,13 @@
+import http from 'http';
+import fs from 'fs';
+
+
+http.createServer((req,res) => {
+    fs.readFile('./practice.html', (err,data) => {
+        if (err) {
+            throw err;
+        }
+        res.end(data) // 읽은 html 파일 버퍼를 있는 그대로 클라이언트에게 전송
+    })
+    
+}).listen(3000,() => console.log('3000 listening!'));
